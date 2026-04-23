b10c903c675f925dff90e4321533abda26fee0a6
jjpxt <jotajunior.peixoto@gmail.com>
2026-04-23 20:55:25 -0300

    Trip Details Page

--- TravelExpenseTracker/Models/TripDetailModel.cs
+++ TravelExpenseTracker/Models/TripDetailModel.cs
@@ -1,6 +1,9 @@
 ﻿namespace TravelExpenseTracker.Models;
 
 public record TripDetailModel(string Image, string Title, string Location, string CategoryName,
-    string Status, DateTime StartDate, DateTime EndDate);
+    string Status, DateTime StartDate, DateTime EndDate)
+{
+    public string DisplayDateRange => $"{StartDate:dd-mm-yy}-{EndDate:dd-mm-yy}";
+}
 
 
--- TravelExpenseTracker/ViewModels/TripDetailsViewModel.cs
+++ TravelExpenseTracker/ViewModels/TripDetailsViewModel.cs
@@ -8,10 +8,13 @@ namespace TravelExpenseTracker.ViewModels;
 public partial class TripDetailsViewModel : ObservableObject
 {
     public TripDetailModel TripInfo { get; set; } = new TripDetailModel(
-        "category_beach.png", "A bitch trip", "Some place", "beach", "Planned",
+        "category_beach.png", "A Beach trip", "Some place", "beach", "Planned",
         DateTime.Now.AddDays(10), DateTime.Now.AddDays(20));
 
     public ObservableCollection<ExpenseModel> Expenses { get; set; } = [];
+
+    [ObservableProperty]
+    private decimal _totalExpenses;
 
     [RelayCommand]
     private void AddExpenseTemp()
@@ -19,5 +22,7 @@ public partial class TripDetailsViewModel : ObservableObject
         Expenses.Add(new ExpenseModel(1, "Flight tickets", "Tickets", 1500, DateTime.Today));
         Expenses.Add(new ExpenseModel(2, "Clothes, Shoes and cosmetic", "Shopping", 100, DateTime.Today));
         Expenses.Add(new ExpenseModel(3, "Breakfast", "Food", 500, DateTime.Now.AddDays(2)));
+
+        TotalExpenses = Expenses.Sum(e => e.Amount);
     }
 }
